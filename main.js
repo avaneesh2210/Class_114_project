d573acc5635c4c24a8d21f870e6ae23cc9fa4ea3
avaneesh2210 <72684066+avaneesh2210@users.noreply.github.com>
2022-05-09 13:49:15 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,33 @@
+nose_x = 0;
+nose_y = 0;
+
 function preload(){
-    
+    mustache = loadImage('https://i.postimg.cc/KjQ1wLfq/mustache.png');
 }
 function setup(){
     canvas=createCanvas(300, 300);
     canvas.center();
+    video=createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+    poseNet=ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+function modelLoaded(){
+    console.log('PoseNet is Initialized');
+}
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results);
+        nose_x = results[0].pose.nose.x;
+        nose_y = results[0].pose.nose.y;
+        console.log("nose x = " + nose_x);
+        console.log("nose y = " + nose_y);
+    }
 }
 function draw(){
-
+    image(video, 0, 0, 300, 300);
+    image(mustache, nose_x - 15, nose_y, 30, 30);
 }
 function take_snapshot(){
     save('myFilterImage.png');
